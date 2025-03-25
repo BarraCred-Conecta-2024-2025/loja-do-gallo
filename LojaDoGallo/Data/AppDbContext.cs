a5f7096ac056b479563eea84bef473da89490e90
gallojunior <gallojunior@gmail.com>
2025-03-25 16:39:45 -0300

    Incluindo usuários

--- LojaDoGallo/Data/AppDbContext.cs
+++ LojaDoGallo/Data/AppDbContext.cs
@@ -1,9 +1,11 @@
 using LojaDoGallo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace LojaDoGallo.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
@@ -11,5 +13,22 @@ public class AppDbContext : DbContext
 
     public DbSet<Categoria> Categorias { get; set; }
     public DbSet<Produto> Produtos { get; set; }
-    public DbSet<ProdutoFoto> ProdutoFotos { get; set; }    
+    public DbSet<ProdutoFoto> ProdutoFotos { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<IdentityUser>().ToTable("Usuario");
+        builder.Entity<IdentityRole>().ToTable("Perfil");
+        builder.Entity<IdentityUserRole<string>>().ToTable("UsuarioPerfil");
+        builder.Entity<IdentityUserClaim<string>>().ToTable("UsuarioRegras");
+        builder.Entity<IdentityUserToken<string>>().ToTable("UsuarioToken");
+        builder.Entity<IdentityUserLogin<string>>().ToTable("UsuarioLogin");
+        builder.Entity<IdentityRoleClaim<string>>().ToTable("PerfilRegras");
+
+        AppDbSeed seed = new(builder);
+    }
+
 }
--- LojaDoGallo/Program.cs
+++ LojaDoGallo/Program.cs
@@ -1,4 +1,6 @@
 using LojaDoGallo.Data;
+using LojaDoGallo.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +15,19 @@ builder.Services.AddDbContext<AppDbContext>(
     options => options.UseSqlServer(conexao)
 );
 
+builder.Services.AddIdentity<Usuario, IdentityRole>(
+    options => options.SignIn.RequireConfirmedEmail = false
+).AddEntityFrameworkStores<AppDbContext>()
+.AddDefaultTokenProviders();
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider
+        .GetRequiredService<AppDbContext>();
+    await db.Database.EnsureCreatedAsync();
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
